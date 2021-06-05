67ce30f495c222d061b4a0045ac564393889edfe
shivam satpute <shivam897satpute@gmail.com>
2021-06-05 08:18:16 +0530

    [shivam]add: Create a Address Book Table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,15 @@
 create database Address_Book_Service
 select * from sys.databases;
 use Address_Book_Service 
+create table Address_Book_Service
+(
+Firstname varchar(15) not null,
+Lastname varchar(15) not null,
+address varchar(30) not null,
+city varchar(15) not null,
+state varchar(15) not null,
+zip int not null,
+phoneNumber varchar(12) not null,
+email varchar(20)	not null,
+);
+select * from Address_Book_Service 
